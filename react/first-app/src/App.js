4c4919e233e240cf4bba363a8d1dc2d253417c02
ZhengjunHUO <firelouiszj@hotmail.com>
2023-12-01 19:43:08 +0100

    add css & using map to reuse components
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

--- react/first-app/src/App.js
+++ react/first-app/src/App.js
@@ -1,10 +1,20 @@
-import { Item } from './components/Item';
+import { Item } from "./components/Item";
 
 function App() {
+  const items = [
+    { date: new Date(2023, 10, 8), name: "购物", sum: "121" },
+    { date: new Date(2023, 10, 9), name: "超市", sum: "67" },
+    { date: new Date(2023, 10, 10), name: "水电", sum: "32" },
+  ];
+
   return (
     <div>
       <h1>Hello React!</h1>
-      <Item></Item>
+      <div>
+        {items.map((item) => (
+          <Item detail={item}></Item>
+        ))}
+      </div>
     </div>
   );
 }
--- react/first-app/src/components/Item.js
+++ react/first-app/src/components/Item.js
@@ -1,3 +1,21 @@
-export function Item() {
-  return <p>Placeholder for an item.</p>
+import "./Item.css";
+
+export function Item(props) {
+  const year = props.detail.date.getFullYear();
+  const month = props.detail.date.toLocaleString("en-US", { month: "long" });
+  const day = props.detail.date.toLocaleString("en-US", { day: "2-digit" });
+
+  return (
+    <div className="item">
+      <div>
+        <div>{year}</div>
+        <div>{month}</div>
+        <div>{day}</div>
+      </div>
+      <div className="item__description">
+        <h2>{props.detail.name}</h2>
+        <div className="item__price">{props.detail.sum}</div>
+      </div>
+    </div>
+  );
 }
