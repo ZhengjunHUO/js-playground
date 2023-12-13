f74fb072af66a8872e6c199d30d9276d34e274e1
ZhengjunHUO <firelouiszj@hotmail.com>
2023-12-13 13:38:07 +0100

    add effect
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

--- react/order-app/src/components/Layout/CartButton.js
+++ react/order-app/src/components/Layout/CartButton.js
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import Icon from "../Cart/Icon";
 import styles from "./CartButton.module.css";
 import { OrderContext } from "../../store/order-context";
@@ -6,14 +6,34 @@ import { OrderContext } from "../../store/order-context";
 export const CartButton = (props) => {
   const ctx = useContext(OrderContext);
 
+  const [needBump, setNeedBump] = useState(false);
+
+  const { products } = ctx;
+
+  const btnStyles = `${styles.button} ${needBump ? styles.bump : ""}`;
+  useEffect(() => {
+    if (products.length === 0) {
+      return;
+    }
+    setNeedBump(true);
+
+    const timer = setTimeout(() => {
+      setNeedBump(false);
+    }, 500);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [products]);
+
   return (
-    <button className={styles.button} onClick={props.onClick}>
+    <button className={btnStyles} onClick={props.onClick}>
       <span className={styles.icon}>
         <Icon />
       </span>
       <span>购物车</span>
       <span className={styles.badge}>
-        {ctx.products.reduce((sum, prod) => {
+        {products.reduce((sum, prod) => {
           return sum + prod.amount;
         }, 0)}
       </span>
