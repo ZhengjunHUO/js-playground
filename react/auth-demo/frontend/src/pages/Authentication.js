b595b4821ee3c0689229eccb916d66752c012a5a
ZhengjunHUO <firelouiszj@hotmail.com>
2023-12-18 14:56:43 +0100

    impl signup
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

---
+++ react/auth-demo/frontend/src/pages/Authentication.js
@@ -0,0 +1,42 @@
+import { json, redirect } from "react-router-dom";
+import AuthForm from "../components/AuthForm";
+
+function AuthenticationPage() {
+  return <AuthForm />;
+}
+
+export default AuthenticationPage;
+
+export const authAction = async ({ request }) => {
+  const searchParams = new URL(request.url).searchParams;
+  const mode = searchParams.get("mode") || "login";
+
+  if (mode !== "login" && mode !== "signup") {
+    throw json({ message: "Unknown mode !" }, { status: 422 });
+  }
+
+  const data = await request.formData();
+  const authData = {
+    email: data.get("email"),
+    password: data.get("password"),
+  };
+
+  const resp = await fetch("http://localhost:8080/" + mode, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(authData),
+  });
+
+  if (resp.status === 422 || resp.status === 401) {
+    return resp;
+  }
+
+  if (!resp.ok) {
+    throw json({ message: "Auth failed !" }, { status: 500 });
+  }
+
+  // TODO deal with token
+  return redirect("/");
+};
