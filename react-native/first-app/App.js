0c3115400d657fa4d92bbe21adba89193c908669
ZhengjunHUO <firelouiszj@hotmail.com>
2023-12-28 18:57:08 +0100

    react native useState & interactive
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

--- react-native/first-app/App.js
+++ react-native/first-app/App.js
@@ -1,14 +1,36 @@
+import { useState } from "react";
 import { StyleSheet, Text, TextInput, View, Button } from "react-native";
 
 export default function App() {
+  const [inputText, setInputText] = useState("");
+  const [textList, setTextList] = useState([]);
+
+  function inputHandler(content) {
+    setInputText(content);
+    console.log(inputText);
+  }
+
+  function pressHandler() {
+    setTextList((prev) => [...prev, inputText]);
+    console.log(textList);
+  }
+
   return (
     <View style={styles.rootContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.inputText} placeholder="Enter here" />
-        <Button title="Go !" />
+        <TextInput
+          style={styles.inputText}
+          placeholder="Enter here"
+          onChangeText={inputHandler}
+        />
+        <Button title="Go !" onPress={pressHandler} />
       </View>
-      <View>
-        <Text>TODO</Text>
+      <View style={styles.showContainer}>
+        {textList.map((item) => (
+          <View key={item} style={styles.showText}>
+            <Text>{item}</Text>
+          </View>
+        ))}
       </View>
     </View>
   );
@@ -16,11 +38,17 @@ export default function App() {
 
 const styles = StyleSheet.create({
   rootContainer: {
+    flex: 1,
     padding: 80,
   },
   inputContainer: {
+    flex: 1,
     flexDirection: "row",
     justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 30,
+    borderBottomWidth: 2,
+    borderBottomColor: "#aaaaaa",
   },
   inputText: {
     borderWidth: 2,
@@ -28,5 +56,14 @@ const styles = StyleSheet.create({
     width: "90%",
     marginRight: 10,
     padding: 5,
+  },
+  showContainer: {
+    flex: 5,
+  },
+  showText: {
+    padding: 5,
+    marginBottom: 5,
+    borderRadius: 5,
+    backgroundColor: "red",
   },
 });
