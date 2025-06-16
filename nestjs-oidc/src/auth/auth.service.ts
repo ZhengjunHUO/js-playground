f6183d4fce279aa6f45760dfb01b0b6916c4ed0a
ZhengjunHUO <firelouiszj@hotmail.com>
2025-06-16 10:36:33 +0200

    introduce oidc client & methods
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

--- nestjs-oidc/src/auth/auth.service.ts
+++ nestjs-oidc/src/auth/auth.service.ts
@@ -1,13 +1,76 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  Injectable,
+  OnModuleInit,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { JwtService } from '@nestjs/jwt';
+import * as client from 'openid-client';
 
 @Injectable()
-export class AuthService {
+export class AuthService implements OnModuleInit {
   constructor(
     private usersService: UsersService,
     private jwtService: JwtService,
   ) {}
+
+  private config: client.Configuration;
+  private codeVerifierMap = new Map<string, string>();
+  private paramsMap = new Map<String, Record<string, string>>();
+
+  async onModuleInit() {
+    this.config = await client.discovery(
+      new URL('http://localhost:8080/realms/oidc'),
+      'oidc-backend',
+      '8cHU783LSC839uhapouji3dHJ34N32SC',
+    );
+  }
+
+  async generateAuthUrl(sessionId: string): Promise<URL> {
+    let codeVerifier: string = client.randomPKCECodeVerifier();
+    let codeChallenge: string =
+      await client.calculatePKCECodeChallenge(codeVerifier);
+    this.codeVerifierMap.set(sessionId, codeVerifier);
+
+    let redirectURL = 'http://127.0.0.1:3000/callback';
+    let scope = 'openid profile email';
+    let codeChallengeMethod = 'S256';
+
+    let params: Record<string, string> = {
+      redirectURL,
+      scope,
+      codeChallenge,
+      codeChallengeMethod,
+    };
+
+    if (!this.config.serverMetadata().supportsPKCE()) {
+      params.state = client.randomState();
+    }
+
+    let redirectTo: URL = client.buildAuthorizationUrl(this.config, params);
+    this.paramsMap.set(sessionId, params);
+    console.log(`Redirect URL: ${redirectTo}`);
+    return redirectTo;
+  }
+
+  async callback(sessionId: string): Promise<client.TokenEndpointResponse> {
+    const codeVerifier = this.codeVerifierMap.get(sessionId);
+    const params = this.paramsMap.get(sessionId);
+    let tokens: client.TokenEndpointResponse =
+      await client.authorizationCodeGrant(
+        this.config,
+        new URL('http://127.0.0.1:3000/callback'),
+        {
+          pkceCodeVerifier: codeVerifier,
+          expectedState: params?.state,
+        },
+      );
+    this.codeVerifierMap.delete(sessionId);
+    this.paramsMap.delete(sessionId);
+
+    console.log('Token Endpoint Response', tokens);
+    return tokens;
+  }
 
   async signIn(
     username: string,
