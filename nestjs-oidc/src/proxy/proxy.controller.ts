86f925cde77f89a39f8e4bf884d1bd419772a5c6
ZhengjunHUO <firelouiszj@hotmail.com>
2025-08-12 11:16:45 +0200

    grab acess token from session store and attach to req's header while proxying
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

--- nestjs-oidc/src/proxy/proxy.controller.ts
+++ nestjs-oidc/src/proxy/proxy.controller.ts
@@ -1,18 +1,27 @@
 import { createProxyMiddleware } from 'http-proxy-middleware';
-import { All, Controller, Next, Req, Res } from '@nestjs/common';
+import { All, Controller, Next, Req, Res, UseGuards } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 // Test with http://127.0.0.1:3000/proxy?url=https%3A%2F%2Fkernel.org
-
+//           http://127.0.0.1:3000/proxy?url=http%3A%2F%2F127.0.0.1%3A8001%2Fapp
 const proxy = createProxyMiddleware({
   router: (req: Request) => {
     return req.query.url as string;
+  },
+  on: {
+    proxyReq: (proxyReq, req: Request, res: Response) => {
+      const session: Record<string, any> = req.session;
+      const access_token = session.tokenSet.access_token;
+      proxyReq.setHeader('authorization', `Bearer ${access_token}`);
+    },
   },
   ignorePath: true,
   changeOrigin: true,
 });
 
 @Controller('proxy')
+@UseGuards(AuthGuard)
 export class ProxyController {
   @All()
   get(@Req() req: Request, @Res() res: Response, @Next() next: NextFunction) {
--- nestjs-oidc/src/proxy/proxy.module.ts
+++ nestjs-oidc/src/proxy/proxy.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ProxyController } from './proxy.controller';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   controllers: [ProxyController],
 })
 export class ProxyModule {}
